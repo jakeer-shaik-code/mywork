0b46c203a4efdcb7971bf0eaa7918dfffe23e846
jakeer-shaik-code <140697803+jakeer-shaik-code@users.noreply.github.com>
2023-09-15 15:34:43 +0530

    Add files via upload

---
+++ employee program.py
@@ -0,0 +1,36 @@
+# general form(single employee)-functions 
+'''def details():
+    empdetails={}
+    name=input("enter a empname:")
+    domain=input("enter a domain:")
+    empid=input("enter a empid:")
+    email=input("enter a email:")
+    empdetails["name"]=name
+    empdetails["domain"]=domain
+    empdetails["empid"]=empid
+    empdetails["mail"]=email
+    print(empdetails)
+details()'''
+
+#multiple employee details-
+
+'''def details():
+    empdetails={}
+    num_of_employee=int(input("enter a num:"))
+    for i in range(num_of_employee):
+        name=input("enter a empname:")
+        domain=input("enter a domain:")
+        empid=input("enter a empid:")
+        email=input("enter a email:")
+        empdetails["name"]=name
+        empdetails["domain"]=domain
+        empdetails["empid"]=empid
+        empdetails["mail"]=email
+        print(empdetails)
+
+details()'''
+
+
+
+
+
