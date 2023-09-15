557f2fd826be7e8d393eeb90c2d2f3e9bb0c1f5a
jakeer-shaik-code <140697803+jakeer-shaik-code@users.noreply.github.com>
2023-09-15 15:39:16 +0530

    Add files via upload

--- employee program.py
+++ employee program.py
@@ -30,6 +30,24 @@ details()'''
 
 details()'''
 
+# multiple employe at  same time
+'''
+def details():
+    empdetails={}
+    num_of_employee=int(input("enter a num:"))
+    for i in range(num_of_employee):
+        name=input("enter a empname:")
+        domain=input("enter a domain:")
+        empid=input("enter a empid:")
+        email=input("enter a email:")
+        empdetails["name"+str(i)]=name
+        empdetails["domain"+str(i)]=domain
+        empdetails["empid"+str(i)]=empid
+        empdetails["mail"+str(i)]=email
+    print(empdetails)
+
+details()'''
+
 
 
 
