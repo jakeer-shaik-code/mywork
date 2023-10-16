167d8b73ceeb533cf224592f0ccb212677472234
jakeer-shaik-code <140697803+jakeer-shaik-code@users.noreply.github.com>
2023-10-16 13:33:12 +0530

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -54,6 +54,82 @@ print(Employees)
 
 fliter_details()'''
 
+######################################## calculator using oops#################################
+
+class Calculator:
+    def __init__(self):
+        print("CALCULATOR")
+        self.display()
+        
+    
+    def display(self):
+        print(''' Select Which operations do you need
+        1.Addition
+        2.Subtarction
+        3.Multipliaction
+        4.Divison
+        Press 1 for Add , 2 for sub, 3 for Mul, 4 for Div''')
+        
+        n = int(input("Enter the value:"))
+        self.numbers = list(map(int,input("Enter number of values:").split()))
+        if(n == 1):
+            self.add(self.numbers)
+        elif(n == 2):
+            self.sub(self.numbers)
+        elif(n == 3):
+            self.mul(self.numbers)
+        elif(n == 4):
+            self.div(self.numbers)    
+        else:
+            print("select one")
+              
+
+    def add(self,numbers):
+        print(f'Sum of numbers is {sum(numbers)}')
+        
+
+    def sub(self,numbers):
+        output = self.numbers[0]
+        for i in self.numbers[1:]:
+           result -= i  
+        print(f'Subtraction of numbers is {output}')
+           
+
+    def mul(self,numbers):
+        output = 1
+        for i in self.numbers[0:]:
+            result *= i 
+        print(f'Multipliaction of numbers is {output}') 
+         
+
+    def div(self,numbers):
+        output = self.numbers[0]
+        for i in self.numbers[1:]:
+           result /= i  
+        print(f'Division of numbers is {output}')
+                 
+              
+c = Calculator()
+print("come again")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
